a7967386d1a039e91f4f8d6eb217ca2ae8c70644
AO Industries, Inc. <support@aoindustries.com>
2019-10-02 02:05:09 -0500

    Reverted to simpler JavaScript-like interface.
    UTF-8 always, encodeURI/decodeURI, encodeURIComponent/decodeURIComponent

--- src/main/java/com/semanticcms/file/servlet/impl/FileImpl.java
+++ src/main/java/com/semanticcms/file/servlet/impl/FileImpl.java
@@ -28,7 +28,7 @@ import static com.aoindustries.encoding.TextInXhtmlAttributeEncoder.encodeTextIn
 import static com.aoindustries.encoding.TextInXhtmlEncoder.encodeTextInXhtml;
 import com.aoindustries.io.buffer.BufferResult;
 import com.aoindustries.net.Path;
-import com.aoindustries.servlet.ServletUtil;
+import com.aoindustries.net.URIEncoder;
 import com.aoindustries.servlet.http.LastModifiedServlet;
 import com.aoindustries.util.StringUtility;
 import com.semanticcms.core.model.NodeBodyWriter;
@@ -143,7 +143,7 @@ final public class FileImpl {
 					;
 				}
 				encodeTextInXhtmlAttribute(
-					response.encodeURL(ServletUtil.encodeURI(urlPath, response)),
+					response.encodeURL(URIEncoder.encodeURI(urlPath)),
 					out
 				);
 			}
