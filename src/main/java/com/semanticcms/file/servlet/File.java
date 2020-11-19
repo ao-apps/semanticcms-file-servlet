e8366995b003fea7cc2e9c738c901dfafae1fbab
AO Industries, Inc <support@aoindustries.com>
2020-11-19 02:56:35 -0600

    New project "ao-encoding-taglib" split from "ao-taglib"

--- src/main/java/com/semanticcms/file/servlet/File.java
+++ src/main/java/com/semanticcms/file/servlet/File.java
@@ -22,13 +22,13 @@
  */
 package com.semanticcms.file.servlet;
 
+import com.aoindustries.encoding.taglib.EncodingBufferedSimpleTag;
 import com.aoindustries.html.servlet.HtmlEE;
 import com.aoindustries.io.buffer.BufferResult;
 import com.aoindustries.io.buffer.BufferWriter;
 import com.aoindustries.lang.Strings;
 import com.aoindustries.net.DomainName;
 import com.aoindustries.net.Path;
-import com.aoindustries.taglib.AutoEncodingBufferedTag;
 import com.semanticcms.core.controller.ResourceRefResolver;
 import com.semanticcms.core.controller.SemanticCMS;
 import com.semanticcms.core.model.ElementContext;
@@ -251,7 +251,7 @@ public class File extends Element<com.semanticcms.file.model.File> {
 		super.doBody(captureLevel, body);
 		BufferWriter capturedOut;
 		if(captureLevel == CaptureLevel.BODY) {
-			capturedOut = AutoEncodingBufferedTag.newBufferWriter(request);
+			capturedOut = EncodingBufferedSimpleTag.newBufferWriter(request);
 		} else {
 			capturedOut = null;
 		}
